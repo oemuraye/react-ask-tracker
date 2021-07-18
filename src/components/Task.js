e86e7ce9d6c6accbc5f7269e0dae2b880efe09d6
oemuraye <oemuraye@gmail.com>
2021-07-18 06:39:56 +0100

    my react project tutorial project

---
+++ src/components/Task.js
@@ -0,0 +1,18 @@
+import * as FaIcons from 'react-icons/fa'
+
+const Task = ({ task, onDelete, onToggle }) => {
+    return (
+        <div className={`task ${task.reminder ? 'reminder' : ''}` } onDoubleClick={() => onToggle(task.id)}>
+            <h3>
+                {task.text}
+                <FaIcons.FaTimes
+                 style={{ color: 'red', cursor: 'pointer' }} 
+                 onClick={() => onDelete(task.id)}
+                />
+            </h3>
+            <p>{task.day}</p>
+        </div>
+    )
+}
+
+export default Task
