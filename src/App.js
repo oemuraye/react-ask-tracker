a2818756c003a54040a9020849ded0c78ba43caf
oemuraye <oemuraye@gmail.com>
2021-07-19 06:43:56 +0100

    Finihed the UI desgning

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
 import { useState } from "react";
+import AddTask from "./components/AddTask";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 const App = () => {
-  const [tasks, setTask] = useState([
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Doctors Appointment',
-        day: 'Feb 6th at 1:30pm',
-        reminder: false,
-    },
-    {
-        id: 3,
-        text: 'Doctors Appointment',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    }
+  const [showAddTask, setShowAddTask] = useState(false)
+
+  const [tasks, setTasks] = useState([
+    // {
+    //     id: 1,
+    //     text: 'Doctors Appointment',
+    //     day: 'Feb 6th at 1:30pm',
+    //     reminder: true,
+    // },
+    // {
+    //     id: 2,
+    //     text: 'Doctors Appointment',
+    //     day: 'Feb 6th at 1:30pm',
+    //     reminder: false,
+    // },
+    // {
+    //     id: 3,
+    //     text: 'Doctors Appointment',
+    //     day: 'Feb 6th at 1:30pm',
+    //     reminder: true,
+    // }
   ])
+
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
 
   //delete task
   const deleteTask = (id) => {
-    setTask(tasks.filter((task) => task.id !== id))
+    setTasks(tasks.filter((task) => task.id !== id))
   }
 
   //Toggle Reminder
   const toggleReminder = (id) => {
-    setTask(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
   }
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
         ) : ( 
